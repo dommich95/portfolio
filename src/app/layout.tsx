4f355e1254c064f2863012dd4c0d62919d442de4
dommich95 <dominikmichel1995@gmail.com>
2023-11-19 15:04:13 +0100

    fix: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         className={inter.className}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
